cf75c9160813cb537da02a2a239de66d12916c13
Shi <shi.digitalnomad@gmail.com>
2017-10-05 17:07:21 +0200

    Add files via upload

---
+++ beginner/script.js
@@ -0,0 +1,38 @@
+/*
+// Image switcher code
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+	} else {
+	  myImage.setAttribute ('src','images/firefox-icon.png');
+	}
+}
+
+// Personalized welcome message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Bitte geben Sie Ihren Namen ein');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla ist cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla ist cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+} */
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hallo Welt!';
